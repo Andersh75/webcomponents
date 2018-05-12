3f02e1a15399757a900e7caa3ad57d6d7b49c170
Andersh75 <andersh75@gmail.com>
2018-05-12 19:33:00 +0200

    Polymer

--- public/js/customelement3.js
+++ public/js/customelement3.js
@@ -9,7 +9,8 @@
 
 // For instance, a focus event does not bubble. There are other examples too, we’ll meet them. But still it’s an exception, rather than a rule, most events do bubble.
 
-import {html, render} from 'https://unpkg.com/lit-html/lib/lit-extended.js?module';
+import { render} from 'https://unpkg.com/lit-html/lib/lit-extended.js?module';
+import {LitElement, html} from '@polymer/lit-element';
 
 class CustomElement3 extends HTMLElement {
 
@@ -27,7 +28,7 @@ class CustomElement3 extends HTMLElement {
 		this.remote = undefined;
 		this.eventDispatcher = eventDispatcherMaker();
 		this.html = html;
-		this.render = render;
+		this.render =render;
 	}
 
 	//-----------
